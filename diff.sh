631c47a96e735d899a3b2f498f9ec5347b8cb9f2
menixator <ahmed.miljau@gmail.com>
2018-09-03 03:39:33 +0500

    Update diff.sh

--- diff.sh
+++ diff.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-diff -u --color=always <(./print_unique_asm.sh $1) <(./print_unique_asm.sh $2)
+diff -u --color=always --label "Test $1" <(./print_unique_asm.sh $1) --label "Test $2" <(./print_unique_asm.sh $2)
 
