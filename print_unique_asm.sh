f271ee088aa79b00006c8e7ee4675891e3622479
menixator <ahmed.miljau@gmail.com>
2018-09-04 16:10:47 +0500

    Update diff.sh

--- print_unique_asm.sh
+++ print_unique_asm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cat tests/asm/Test$1.asm | awk '{ if ($0 !~ /:/ ){ if (NF >= 2) { print $1 " " substr($2, 1,1) } else {print $1}}}' | sort -u
+cat tests/asm/Test$1.asm | awk '{ if ($0 ~ /:/ ){next} if (NF >= 2) { print $1 " " substr($2, 1,1) } else {print $1}}' | sort -u
